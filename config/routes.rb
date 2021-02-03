cf406f1fae932c0653d5a95b8159e4ab9e2f72d0
ryouichi0618 <tennis.ryouichi@gmail.com>
2021-02-03 11:34:09 +0000

    [Add] blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :blogs
 end
 
-
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:ryouichi0618/CARAVAN_app.git
-git push -u origin main
